872f029a8ba2c284b3bf65a9380b3ef4821a66e8
Gabriel Luders <ludersdev@gmail.com>
2022-04-22 14:30:07 -0300

    fix: fixed general time graph

--- rosenbrock/scripts/times/teste.py
+++ rosenbrock/scripts/times/teste.py
@@ -9,7 +9,12 @@ def main():
 
   df = pd.read_csv("generalTime.csv")
   print(df)
-  df.plot(x = 'N', y='n_newtonDefault', kind = 'line')
+  df.plot(
+    x = 'N', 
+    y=['n_newtonDefault', 'o_newtonDefault', 'n_newtonGS', 'o_newtonGS'], 
+    kind = 'line',
+    title= 'Tempo Geral (segundos)',
+  )
   plt.show()
 
 if __name__ == "__main__":
