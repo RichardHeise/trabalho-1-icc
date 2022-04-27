3433e55dc7053218cbb97092ab376dd314577a05
Richard Heise <richardfhf5@gmail.com>
2022-04-26 23:14:19 -0300

    L3 script done

--- scripts/times/time.sh
+++ scripts/times/time.sh
@@ -5,12 +5,13 @@ ALG=$1
 OUTPUT_NAME=$2
 SEARCH=$3
 INPUT_NAME=$4
+SIZE=$5
 
 echo "N,n_${ALG},o_${ALG}" > $OUTPUT_NAME.csv
 for N in 10 32 50 64
 do
-  FIRST_COLUMN=$(cat ../${INPUT_NAME}.dat | grep -i -A 9 n_${ALG}_$N | grep -i "$SEARCH" | head -n 1 | cut -d , -f 2)
-  SECOND_COLUMN=$(cat ../${INPUT_NAME}.dat | grep -i -A 9 o_${ALG}_$N | grep -i "$SEARCH" | head -n 1 | cut -d , -f 2)
+  FIRST_COLUMN=$(cat ../${INPUT_NAME}.dat | grep -i -A $SIZE n_${ALG}_$N | grep -i "$SEARCH" | head -n 1 | cut -d , -f 2)
+  SECOND_COLUMN=$(cat ../${INPUT_NAME}.dat | grep -i -A $SIZE o_${ALG}_$N | grep -i "$SEARCH" | head -n 1 | cut -d , -f 2)
   
   echo "$N,${FIRST_COLUMN},${SECOND_COLUMN}" >> $OUTPUT_NAME.csv
 done
