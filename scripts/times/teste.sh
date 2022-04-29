2747730dd9703f6c44f75084b8c00ccf21e7ac87
RICHARD FERNANDO HEISE FERREIRA <rfhf19@inf.ufpr.br>
2022-04-29 10:00:43 -0300

    script do dp feito

---
+++ scripts/times/teste.sh
@@ -0,0 +1,16 @@
+ALG=$1
+OUTPUT_NAME=$2
+INPUT_NAME=$3
+SIZE=$4
+
+echo "N,n_${ALG}_DP,o_${ALG}_DP,n_${ALG}_AVX,o_${ALG}_AVX" > $OUTPUT_NAME.csv
+for N in 10 32 50 64 100 128 200 250 256 300 400 512 600 1000 1024 2000 2048 3000 4096
+do
+  FIRST_COLUMN=$(cat ../${INPUT_NAME}.dat | grep -i -A $SIZE n_${ALG}_$N | grep -i "^DP" | head -n 1 | cut -d , -f 2)
+  SECOND_COLUMN=$(cat ../${INPUT_NAME}.dat | grep -i -A $SIZE o_${ALG}_$N | grep -i "^DP" | head -n 1 | cut -d , -f 2)
+  THIRD_COLUMN=$(cat ../${INPUT_NAME}.dat | grep -i -A $SIZE n_${ALG}_$N | grep -i "^AVX" | head -n 1 | cut -d , -f 2)
+  FOURTH_COLUMN=$(cat ../${INPUT_NAME}.dat | grep -i -A $SIZE o_${ALG}_$N | grep -i "^AVX" | head -n 1 | cut -d , -f 2)
+
+  echo "$N,${FIRST_COLUMN},${SECOND_COLUMN},${THIRD_COLUMN},${FOURTH_COLUMN}" >> $OUTPUT_NAME.csv
+done
+
