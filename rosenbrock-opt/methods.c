51232ca3aca80b3a25c267f49eac4f5777242007
Richard Heise <richardfhf5@gmail.com>
2022-04-30 12:53:00 -0300

    gradient com unroll

--- rosenbrock-opt/methods.c
+++ rosenbrock-opt/methods.c
@@ -107,7 +107,20 @@ void calcHessian(sl* linSys) {
 /* ====================================================================================== */
 
 void calcGradient(sl* linSys) {
-  for(int i = 0; i < linSys->d; i++){
+  int i;
+  for(i = 0; i < linSys->d - (linSys->d % 4); i += 4){
+    linSys->Gi[i] = rosenbrock_dx(i, linSys->Xi, linSys->d);
+    linSys->Gi[i + 1] = rosenbrock_dx(i + 1, linSys->Xi, linSys->d);
+    linSys->Gi[i + 2] = rosenbrock_dx(i + 2, linSys->Xi, linSys->d);
+    linSys->Gi[i + 3] = rosenbrock_dx(i + 3, linSys->Xi, linSys->d);
+
+    linSys->nGi[i] = (-1*linSys->Gi[i]);
+    linSys->nGi[i + 1] = (-1*linSys->Gi[i + 1]);
+    linSys->nGi[i + 2] = (-1*linSys->Gi[i + 2]);
+    linSys->nGi[i + 3] = (-1*linSys->Gi[i + 3]);
+  }
+
+  for(i; i < linSys->d; i++){
     linSys->Gi[i] = rosenbrock_dx(i, linSys->Xi, linSys->d);
 
     linSys->nGi[i] = (-1*linSys->Gi[i]);
