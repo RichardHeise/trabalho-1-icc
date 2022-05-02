687b6f3b7d260f9eb822084556f4310d5add98c6
Gabriel Luders <ludersdev@gmail.com>
2022-05-01 23:45:31 -0300

    teste

--- rosenbrock-opt/methods.c
+++ rosenbrock-opt/methods.c
@@ -91,46 +91,46 @@ void retroSub(sl* linSys){
 
 /* ====================================================================================== */
 
-// void calcHessian(sl* linSys) {
-//   int i, j, ii, jj, istart, iend, jstart, jend;
-
-//   for(ii = 0; ii < linSys->d / B_SIZE; ii++){
-//     istart = ii * B_SIZE; iend = istart + B_SIZE;
-//     for(jj = 0; jj < linSys->d / B_SIZE; jj++){
-//       jstart = jj * B_SIZE; jend = jstart + B_SIZE;
-
-//         for (i = istart; i < iend; i++) {
-//           for (j = jstart; j < jend; j += 4) {
-//             linSys->Hi[i][j] = rosenbrock_dxdy(i, j, linSys->Xi, linSys->d); 
-//             linSys->Hi[i][j + 1] = rosenbrock_dxdy(i, j + 1, linSys->Xi, linSys->d); 
-//             linSys->Hi[i][j + 2] = rosenbrock_dxdy(i, j + 2, linSys->Xi, linSys->d); 
-//             linSys->Hi[i][j + 3] = rosenbrock_dxdy(i, j + 3, linSys->Xi, linSys->d); 
-//           }
-//         }
-//     }
-//     jstart = jj * B_SIZE;
-//     for (i = istart; i < iend; i++) {
-//       for (j = jstart; j < linSys->d; j++) {
-//         linSys->Hi[i][j] = rosenbrock_dxdy(i, j, linSys->Xi, linSys->d); 
-//       }
-//     }
-//   }
-
-//   for(i = ii * B_SIZE; i < linSys->d; i++){
-//     for(j = 0; j < linSys->d; j++){
-//       linSys->Hi[i][j] = rosenbrock_dxdy(i, j, linSys->Xi, linSys->d); 
-//     }
-//   }
-  
-// }
-
 void calcHessian(sl* linSys) {
-  for (int i = 0; i < linSys->d; i++) {
-    for (int j = 0; j < linSys->d; j++) {
+  int i, j, ii, jj, istart, iend, jstart, jend;
+
+  for(ii = 0; ii < linSys->d / B_SIZE; ii++){
+    istart = ii * B_SIZE; iend = istart + B_SIZE;
+    for(jj = 0; jj < linSys->d / B_SIZE; jj++){
+      jstart = jj * B_SIZE; jend = jstart + B_SIZE;
+
+        for (i = istart; i < iend; i++) {
+          for (j = jstart; j < jend; j += 4) {
+            linSys->Hi[i][j] = rosenbrock_dxdy(i, j, linSys->Xi, linSys->d); 
+            linSys->Hi[i][j + 1] = rosenbrock_dxdy(i, j + 1, linSys->Xi, linSys->d); 
+            linSys->Hi[i][j + 2] = rosenbrock_dxdy(i, j + 2, linSys->Xi, linSys->d); 
+            linSys->Hi[i][j + 3] = rosenbrock_dxdy(i, j + 3, linSys->Xi, linSys->d); 
+          }
+        }
+    }
+    jstart = jj * B_SIZE;
+    for (i = istart; i < iend; i++) {
+      for (j = jstart; j < linSys->d; j++) {
+        linSys->Hi[i][j] = rosenbrock_dxdy(i, j, linSys->Xi, linSys->d); 
+      }
+    }
+  }
+
+  for(i = ii * B_SIZE; i < linSys->d; i++){
+    for(j = 0; j < linSys->d; j++){
       linSys->Hi[i][j] = rosenbrock_dxdy(i, j, linSys->Xi, linSys->d); 
     }
   }
+  
 }
+
+// void calcHessian(sl* linSys) {
+//   for (int i = 0; i < linSys->d; i++) {
+//     for (int j = 0; j < linSys->d; j++) {
+//       linSys->Hi[i][j] = rosenbrock_dxdy(i, j, linSys->Xi, linSys->d); 
+//     }
+//   }
+// }
 
 /* ====================================================================================== */
 
@@ -167,70 +167,6 @@ void solveSL(sl* linSys) {
 
 /* ====================================================================================== */
 
-void gaussSeidel(sl* linSys) {
-  int n = linSys->d;
-  
-  // 0 in deltai array
-  memset(linSys->deltai, 0, sizeof(double)*linSys->d);
-  double **A = linSys->Hi;
-  double *B = linSys->nGi;
-  double *X = linSys->deltai;
-  double error = 10e-6;
-
-  int k, i, j;
-  double xk, norm, diff = 0;
-  double s[4], soma;
-  norm=1.0+error;
-
-  for (k=0; norm > error; ++k) {
-    norm = 0.0;
-
-    for (i=0; i < n; ++i) {
-      s[0] = s[1] = s[2] = s[3] = 0.0;
-      soma = 0.0;
-
-      
-      fprintf(stderr, "sum: %f\n", s[0]);
-      fprintf(stderr, "sum: %f\n", s[1]);
-      fprintf(stderr, "sum: %f\n", s[2]);
-      fprintf(stderr, "sum: %f\n", s[3]);
-
-      for (j=0; j < i - (i % 4); j+=4){
-        s[0] += A[i][j] * X[j];
-        s[1] += A[i][j + 1] * X[j + 1];
-        s[2] += A[i][j + 2] * X[j + 2];
-        s[3] += A[i][j + 3] * X[j + 3];
-      }
-
-      for(; j < i; j++)
-        s[0] += A[i][j] * X[j];
-
-      for (j=i+1; j < n - (n % 4); j += 4){
-        s[0] += A[i][j] * X[j];
-        s[1] += A[i][j + 1] * X[j + 1];
-        s[2] += A[i][j + 2] * X[j + 2];
-        s[3] += A[i][j + 3] * X[j + 3];
-      }
-
-      for(; j < n; j++)
-        s[0] += A[i][j] * X[j];
-
-      soma += s[0] + s[1] + s[2] + s[3];
-      fprintf(stderr, "sum: %f\n", s[0]);
-      fprintf(stderr, "sum: %f\n", s[1]);
-      fprintf(stderr, "sum: %f\n", s[2]);
-      fprintf(stderr, "sum: %f\n\n", s[3]);
-
-      checkZeroDivision(A[i][i], linSys->f->strFunc, __func__);
-      xk = (B[i] - soma) / A[i][i];
-      diff = fabs(xk - X[i]);
-
-      if (diff > norm) norm = diff;
-      X[i] = xk;
-    }
-  }
-}
-
 // void gaussSeidel(sl* linSys) {
 //   int n = linSys->d;
   
@@ -242,21 +178,51 @@ void gaussSeidel(sl* linSys) {
 //   double error = 10e-6;
 
 //   int k, i, j;
-//   double s, xk, norm, diff = 0;
+//   double xk, norm, diff = 0;
+//   double s[4], soma;
 //   norm=1.0+error;
 
 //   for (k=0; norm > error; ++k) {
 //     norm = 0.0;
 
 //     for (i=0; i < n; ++i) {
-//       for (s=0, j=0; j < i; ++j) s += A[i][j] * X[j];
+//       s[0] = s[1] = s[2] = s[3] = 0.0;
+//       soma = 0.0;
 
-//       for (j=i+1; j < n; ++j) s += A[i][j] * X[j];
+      
+//       fprintf(stderr, "sum: %f\n", s[0]);
+//       fprintf(stderr, "sum: %f\n", s[1]);
+//       fprintf(stderr, "sum: %f\n", s[2]);
+//       fprintf(stderr, "sum: %f\n", s[3]);
 
-//       fprintf(stderr, "sum: %f\n", s);
+//       for (j=0; j < i - (i % 4); j+=4){
+//         s[0] += A[i][j] * X[j];
+//         s[1] += A[i][j + 1] * X[j + 1];
+//         s[2] += A[i][j + 2] * X[j + 2];
+//         s[3] += A[i][j + 3] * X[j + 3];
+//       }
+
+//       for(; j < i; j++)
+//         s[0] += A[i][j] * X[j];
+
+//       for (j=i+1; j < n - (n % 4); j += 4){
+//         s[0] += A[i][j] * X[j];
+//         s[1] += A[i][j + 1] * X[j + 1];
+//         s[2] += A[i][j + 2] * X[j + 2];
+//         s[3] += A[i][j + 3] * X[j + 3];
+//       }
+
+//       for(; j < n; j++)
+//         s[0] += A[i][j] * X[j];
+
+//       soma += s[0] + s[1] + s[2] + s[3];
+//       fprintf(stderr, "sum: %f\n", s[0]);
+//       fprintf(stderr, "sum: %f\n", s[1]);
+//       fprintf(stderr, "sum: %f\n", s[2]);
+//       fprintf(stderr, "sum: %f\n\n", s[3]);
 
 //       checkZeroDivision(A[i][i], linSys->f->strFunc, __func__);
-//       xk = (B[i] - s) / A[i][i];
+//       xk = (B[i] - soma) / A[i][i];
 //       diff = fabs(xk - X[i]);
 
 //       if (diff > norm) norm = diff;
@@ -264,6 +230,40 @@ void gaussSeidel(sl* linSys) {
 //     }
 //   }
 // }
+
+void gaussSeidel(sl* linSys) {
+  int n = linSys->d;
+  
+  // 0 in deltai array
+  memset(linSys->deltai, 0, sizeof(double)*linSys->d);
+  double **A = linSys->Hi;
+  double *B = linSys->nGi;
+  double *X = linSys->deltai;
+  double error = 10e-6;
+
+  int k, i, j;
+  double s, xk, norm, diff = 0;
+  norm=1.0+error;
+
+  for (k=0; norm > error; ++k) {
+    norm = 0.0;
+
+    for (i=0; i < n; ++i) {
+      for (s=0, j=0; j < i; ++j) s += A[i][j] * X[j];
+
+      for (j=i+1; j < n; ++j) s += A[i][j] * X[j];
+
+      fprintf(stderr, "sum: %f\n", s);
+
+      checkZeroDivision(A[i][i], linSys->f->strFunc, __func__);
+      xk = (B[i] - s) / A[i][i];
+      diff = fabs(xk - X[i]);
+
+      if (diff > norm) norm = diff;
+      X[i] = xk;
+    }
+  }
+}
 
 /* ====================================================================================== */
 
