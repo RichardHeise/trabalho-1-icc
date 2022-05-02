412c4a8baa2a2cbee58ea8d3c0f806e9d370cc17
Gabriel Luders <ludersdev@gmail.com>
2022-05-02 00:33:09 -0300

    teste

--- rosenbrock-opt/methods.c
+++ rosenbrock-opt/methods.c
@@ -237,7 +237,7 @@ void gaussSeidel(sl* linSys) {
 
       for (j=i+1; j < n; ++j) s += A[i][j] * X[j];
 
-      checkZeroDivision(A[i][i], linSys->f->strFunc);
+      checkZeroDivision(A[i][i], linSys->f->strFunc, __func__);
       xk = (B[i] - s) / A[i][i];
       diff = fabs(xk - X[i]);
 
